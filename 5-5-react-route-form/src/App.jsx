721767e1782ac7ec4dbfe01a3ac9ca9f05a7a250
jawwad183 <mjwdmufti@gmail.com>
2025-10-12 11:03:48 +0300

    Update in code

--- 5-5-react-route-form/src/App.jsx
+++ 5-5-react-route-form/src/App.jsx
@@ -144,36 +144,18 @@
 // ================================================================
 
 
-import { Routes, Route, NavLink } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Registration from "./pages/Registration";
-
 export default function App() {
   return (
     <div className="app">
       <nav className="navbar">
         <div className="brand">🧑‍💻 Student Portal</div>
         <div className="links">
-          <NavLink to="/" end className="navlink">
-            Home
-          </NavLink>
-          <NavLink to="/about" className="navlink">
-            About
-          </NavLink>
-          <NavLink to="/registration" className="navlink">
-            Registration
-          </NavLink>
+          {/*Nav links*/}
         </div>
       </nav>
 
       <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="*" element={<h2>404 — Not Found</h2>} />
-        </Routes>
+        {/*Routes*/}
       </main>
 
       <footer className="footer">
